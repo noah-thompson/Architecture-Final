252333c21f9f04fc1cbbe4655c541f6e6f2a08a1
Noah Thompson <noah.thompson@mymail.champlain.edu>
2022-12-09 11:11:16 -0500

    Worked on final

--- Final Project/Final Project/Text.asm
+++ Final Project/Final Project/Text.asm
@@ -1,18 +1,71 @@
 extrn ExitProcess : proc
 
 node STRUCT
-	value DWORD ?
-	left  DWORD ?
-	right DWORD ?
+	value QWORD ?
+	left  QWORD ?
+	right QWORD ?
 node ENDS
 
 .DATA
 head node <10, ?, ?>
+intArr QWORD ?,?,?,?
+space QWORD 4
 
 .CODE
+inOrder PROC var1:QWORD
+
+xor rax, rax
+mov rsi, space
+mov rdi, 0
+
+
+mov rdx, var1
+mov rdx, [rdx+4]
+cmp rdx, 0
+jnz left
+mov rdx, [rdx+8]
+cmp rdx, 0
+jnz right
+jmp value
+
+
+parent:
+	
+
+headNode:
+	mov  rdx, var1
+
+value:
+	mov rax, [rdx] ;  value
+	jmp final
+
+left:
+	;if left go left else
+	mov rbx, [rdx+4]; left address
+	mov rbx, [rbx]
+	mov intArr, rbx;   left value
+	add rdi, rsi
+	;if right go right else parent
+	jmp parent
+right:
+	;if left go left else
+	mov rcx, [rdx+12]; right address
+	mov rax, [rdx];    right value
+	add rdi, rsi
+	;if right go right else parent
+	jmp final
+
+final:
+	
+
+ret
+
+inOrder ENDP
+
+
 asmMain PROC
 
-mov eax, head.value
+mov rax, head.value
 ret
 asmMain ENDP
 END
--- Final Project/Final Project/main.cpp
+++ Final Project/Final Project/main.cpp
@@ -1,11 +1,56 @@
 // main.cpp
 #include <iostream>
 #include <ostream>
-extern "C" int asmMain();
 using namespace std;
+
+#pragma pack(1)
+struct Node
+{
+	int value;
+	Node *left;
+	Node *right;
+	Node()
+	{
+		value = 0;
+		left = NULL;
+		right = NULL;
+	};
+	Node(int val)
+	{
+		value = val;
+		left = NULL;
+		right = NULL;
+	};
+	Node* addNode(Node* node, int val)
+	{
+		if (!node)
+		{
+			return new Node(val);
+		}
+		if (val < node->value)
+		{
+			node->left = addNode(node->left, val);
+		}
+		else
+		{
+			node->right = addNode(node->right, val);
+		}
+
+		return node;
+	};
+};
+extern "C" int asmMain();
+extern "C" int inOrder(Node*);
+
 int main() 
 {
-	int num = asmMain();
+	int num;
+	Node head = Node(10);
+	// head.addNode(&head, 5);
+	//head.addNode(&head, 3);
+	//head.addNode(&head, 15);
+	//num = asmMain();
+	num = inOrder(&head);
 	cout << num;
 	return 0;
 }
