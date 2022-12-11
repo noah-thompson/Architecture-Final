e44c479cbc34ae42cba9f1ece6c419b8994e031d
Noah Thompson <noah.thompson@mymail.champlain.edu>
2022-12-10 23:47:59 -0500

    Pre order poster order, in order and node count

--- Final Project/Final Project/Text.asm
+++ Final Project/Final Project/Text.asm
@@ -1,71 +1,425 @@
 extrn ExitProcess : proc
 
-node STRUCT
-	value QWORD ?
-	left  QWORD ?
-	right QWORD ?
-node ENDS
-
 .DATA
-head node <10, ?, ?>
-intArr QWORD ?,?,?,?
-space QWORD 4
+intArr QWORD 0,0,0,0,0,0,0,0,0,0
+space QWORD 8
+count DWORD 0
+node DWORD 0
 
 .CODE
-inOrder PROC var1:QWORD
+nodeCount PROC var1:QWORD
 
 xor rax, rax
+xor rbx, rbx
 mov rsi, space
 mov rdi, 0
 
 
-mov rdx, var1
-mov rdx, [rdx+4]
+mov rdx, rcx
+mov rbx, [rdx]
+mov count, ebx
+mov rcx, [rdx+4]
 cmp rdx, 0
 jnz left
-mov rdx, [rdx+8]
+mov rcx, [rdx+8]
 cmp rdx, 0
 jnz right
-jmp value
-
-
-parent:
-	
-
-headNode:
-	mov  rdx, var1
-
-value:
-	mov rax, [rdx] ;  value
-	jmp final
+jmp final
 
 left:
-	;if left go left else
-	mov rbx, [rdx+4]; left address
-	mov rbx, [rbx]
-	mov intArr, rbx;   left value
-	add rdi, rsi
-	;if right go right else parent
-	jmp parent
+	mov rdx, [rdx+4]
+
+	;check left
+	mov rcx, [rdx+4]
+	cmp rcx, 0
+	jnz left
+
+	;add current
+	mov rax, [rdx]
+	add node, 1
+
+	;check right
+	mov rcx, [rdx+12]
+	cmp rcx, 0
+	jnz right
+
+
+	;return to parent
+	jmp parentAfterLeft
+
 right:
-	;if left go left else
-	mov rcx, [rdx+12]; right address
-	mov rax, [rdx];    right value
-	add rdi, rsi
-	;if right go right else parent
+	mov rdx, [rdx+12]
+
+	;check left
+	mov rcx, [rdx+4]
+	cmp rcx, 0
+	jnz left
+
+	;add current
+	mov rax, [rdx]
+	add node, 1
+
+	;check right
+	mov rcx, [rdx+12]
+	cmp rcx, 0
+	jnz right
+
+
+	;return to parent
+	jmp parentAfterRight
+
+parentAfterLeft:
+	mov rdx, [rdx+20]
+
+	;addcurrent
+	mov rax, [rdx]
+	add node, 1
+	
+	;check right
+	mov rcx, [rdx+12]
+	cmp rcx, 0
+	jnz right
+
+	;check parent
+	mov rcx, [rdx+20]
+	cmp ecx, count
+	jl parentAfterLeft
 	jmp final
 
-final:
-	
+parentAfterRight:
+	mov rdx, [rdx+20]
+	mov rcx, [rdx]
+	cmp ecx, count
+	jg final
+	jmp parentAfterLeft
 
+
+final:
+	mov eax, node
+
+xor rcx, rcx
+ret
+nodeCount ENDP
+
+
+
+
+inOrder PROC var1:QWORD, var2:DWORD
+
+mov var2, edx
+xor rbx, rbx
+mov rsi, space
+mov rdi, 0
+
+
+mov rdx, rcx
+mov rbx, [rdx]
+mov count, ebx
+mov rcx, [rdx+4]
+cmp rdx, 0
+jnz left
+mov rcx, [rdx+8]
+cmp rdx, 0
+jnz right
+jmp final
+
+left:
+	mov rdx, [rdx+4]
+
+	;check left
+	mov rcx, [rdx+4]
+	cmp rcx, 0
+	jnz left
+
+	;add current
+	mov rax, [rdx]
+	mov [intArr + rdi], rax
+	add rdi, rsi
+
+	;check right
+	mov rcx, [rdx+12]
+	cmp rcx, 0
+	jnz right
+
+
+	;return to parent
+	jmp parentAfterLeft
+
+right:
+	mov rdx, [rdx+12]
+
+	;check left
+	mov rcx, [rdx+4]
+	cmp rcx, 0
+	jnz left
+
+	;add current
+	mov rax, [rdx]
+	mov [intArr + rdi], rax
+	add rdi, rsi
+
+	;check right
+	mov rcx, [rdx+12]
+	cmp rcx, 0
+	jnz right
+
+
+	;return to parent
+	jmp parentAfterRight
+
+parentAfterLeft:
+	mov rdx, [rdx+20]
+
+	;addcurrent
+	mov rax, [rdx]
+	mov [intArr + rdi], rax
+	add rdi, rsi
+	
+	;check right
+	mov rcx, [rdx+12]
+	cmp rcx, 0
+	jnz right
+
+	;check parent
+	mov rcx, [rdx+20]
+	cmp ecx, count
+	jl parentAfterLeft;
+	jmp final
+
+parentAfterRight:
+	mov rdx, [rdx+20]
+	mov rcx, [rdx]
+	cmp ecx, count
+	jg final
+	jmp parentAfterLeft
+
+
+final:
+	lea rcx, intArr
+	mov esp, var2
+	mov rax, [rcx + rsp]
+
+xor rcx, rcx
 ret
 
 inOrder ENDP
 
 
-asmMain PROC
 
-mov rax, head.value
+postOrder PROC var1:QWORD, var2:DWORD
+
+mov var2, edx
+xor rbx, rbx
+mov rsi, space
+mov rdi, 0
+
+
+mov rdx, rcx
+mov rbx, [rdx]
+mov count, ebx
+mov rcx, [rdx+4]
+cmp rdx, 0
+jnz left
+mov rcx, [rdx+8]
+cmp rdx, 0
+jnz right
+jmp final
+
+left:
+	mov rdx, [rdx+4]
+
+	;check left
+	mov rcx, [rdx+4]
+	cmp rcx, 0
+	jnz left
+
+	;check right
+	mov rcx, [rdx+12]
+	cmp rcx, 0
+	jnz right
+
+	;add current
+	mov rax, [rdx]
+	mov [intArr + rdi], rax
+	add rdi, rsi
+
+
+	;return to parent
+	jmp parentAfterLeft
+
+right:
+	mov rdx, [rdx+12]
+
+	;check left
+	mov rcx, [rdx+4]
+	cmp rcx, 0
+	jnz left
+
+	;check right
+	mov rcx, [rdx+12]
+	cmp rcx, 0
+	jnz right
+
+	;add current
+	mov rax, [rdx]
+	mov [intArr + rdi], rax
+	add rdi, rsi
+
+
+	;return to parent
+	jmp parentAfterRight
+
+parentAfterLeft:
+	mov rdx, [rdx+20]
+	
+	;check right
+	mov rcx, [rdx+12]
+	cmp rcx, 0
+	jnz right
+
+	;addcurrent
+	mov rax, [rdx]
+	mov [intArr + rdi], rax
+	add rdi, rsi
+
+	;check parent
+	mov rcx, [rdx+20]
+	cmp ecx, count
+	jl parentAfterLeft;
+	jmp final
+
+parentAfterRight:
+	mov rdx, [rdx+20]
+
+	;addCurrent
+	mov rax, [rdx]
+	mov [intArr + rdi], rax
+	add rdi, rsi
+
+	mov rcx, [rdx]
+	cmp ecx, count
+	jg final
+	jmp parentAfterLeft
+
+
+final:
+	mov [intArr + rdi], rbx
+	add rdi, rsi
+	lea rcx, intArr
+	mov esp, var2
+	mov rax, [rcx + rsp]
+
+xor rcx, rcx
 ret
-asmMain ENDP
+
+postOrder ENDP
+
+
+
+preOrder PROC var1:QWORD, var2:DWORD
+
+mov var2, edx
+xor rbx, rbx
+mov rsi, space
+mov rdi, 0
+
+
+mov rdx, rcx
+
+;add head
+mov rax, [rdx]
+mov [intArr + rdi], rax
+add rdi, rsi
+
+;check left
+mov rbx, [rdx]
+mov count, ebx
+mov rcx, [rdx+4]
+cmp rdx, 0
+jnz left
+
+;check right
+mov rcx, [rdx+8]
+cmp rdx, 0
+jnz right
+jmp final
+
+left:
+	mov rdx, [rdx+4]
+
+	;add current
+	mov rax, [rdx]
+	mov [intArr + rdi], rax
+	add rdi, rsi
+
+	;check left
+	mov rcx, [rdx+4]
+	cmp rcx, 0
+	jnz left
+
+	;check right
+	mov rcx, [rdx+12]
+	cmp rcx, 0
+	jnz right
+
+
+	;return to parent
+	jmp parentAfterLeft
+
+right:
+	mov rdx, [rdx+12]
+
+	;add current
+	mov rax, [rdx]
+	mov [intArr + rdi], rax
+	add rdi, rsi
+
+	;check left
+	mov rcx, [rdx+4]
+	cmp rcx, 0
+	jnz left
+
+	;check right
+	mov rcx, [rdx+12]
+	cmp rcx, 0
+	jnz right
+
+	;return to parent
+	jmp parentAfterRight
+
+parentAfterLeft:
+	mov rdx, [rdx+20]
+	
+	;check right
+	mov rcx, [rdx+12]
+	cmp rcx, 0
+	jnz right
+
+	;check parent
+	mov rcx, [rdx+20]
+	cmp ecx, count
+	jl parentAfterLeft;
+	jmp final
+
+parentAfterRight:
+	mov rdx, [rdx+20]
+
+	mov rcx, [rdx]
+	cmp ecx, count
+	jg final
+	jmp parentAfterLeft
+
+
+final:
+	lea rcx, intArr
+	mov esp, var2
+	mov rax, [rcx + rsp]
+
+xor rcx, rcx
+ret
+
+preOrder ENDP
+
+
+
 END
--- Final Project/Final Project/main.cpp
+++ Final Project/Final Project/main.cpp
@@ -9,48 +9,99 @@ struct Node
 	int value;
 	Node *left;
 	Node *right;
+	Node *parent;
 	Node()
 	{
 		value = 0;
 		left = NULL;
 		right = NULL;
+		parent = NULL;
 	};
 	Node(int val)
 	{
 		value = val;
 		left = NULL;
 		right = NULL;
+		parent = NULL;
+	}
+	Node(int val, Node* nodes)
+	{
+		value = val;
+		left = NULL;
+		right = NULL;
+		parent = nodes;
 	};
-	Node* addNode(Node* node, int val)
+	Node* addNode(Node* node, int val, Node* curr)
 	{
 		if (!node)
 		{
-			return new Node(val);
+			return new Node(val, curr);
 		}
 		if (val < node->value)
 		{
-			node->left = addNode(node->left, val);
+			node->left = addNode(node->left, val,node);
 		}
 		else
 		{
-			node->right = addNode(node->right, val);
+			node->right = addNode(node->right, val,node);
 		}
 
 		return node;
 	};
 };
-extern "C" int asmMain();
-extern "C" int inOrder(Node*);
 
-int main() 
+extern "C" int __stdcall inOrder(Node*, int);
+extern "C" int __stdcall postOrder(Node*, int);
+extern "C" int __stdcall preOrder(Node*, int);
+extern "C" int __stdcall nodeCount(Node*);
+
+int main()
 {
-	int num;
+	int num = 0;
 	Node head = Node(10);
-	// head.addNode(&head, 5);
-	//head.addNode(&head, 3);
-	//head.addNode(&head, 15);
-	//num = asmMain();
-	num = inOrder(&head);
-	cout << num;
+	head.addNode(&head, 5, &head);
+	head.addNode(&head, 3, &head);
+	head.addNode(&head, 7, &head);
+	head.addNode(&head, 15, &head);
+	head.addNode(&head, 12, &head);
+	head.addNode(&head, 20, &head);
+
+	//count
+	int count = nodeCount(&head);
+	cout << "Count: " << count << endl;
+
+	//In order
+	cout << "In Order: ";
+	for (int i = 0; i < count; ++i)
+	{
+		i *= 8;
+		num = inOrder(&head, i);
+		cout << num << " ";
+		i /= 8;
+	}
+	cout << endl;
+	
+	//Post order
+	cout << "Post Order: ";
+	for (int i = 0; i < count; ++i)
+	{
+		i *= 8;
+		num = postOrder(&head, i);
+		cout << num << " ";
+		i /= 8;
+	}
+	cout << endl;
+	
+	//Pre order
+	cout << "Pre Order: ";
+	for (int i = 0; i < count; ++i)
+	{
+		i *= 8;
+		num = preOrder(&head, i);
+		cout << num << " ";
+		i /= 8;
+	}
+	cout << endl;
+
 	return 0;
 }
